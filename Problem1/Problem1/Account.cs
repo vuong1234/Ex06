de422079b89a930786bf68a1e259575c9b624304
vuong <vuongcmm1802@gmail.com>
2019-10-15 17:53:30 +0700

    vuong

---
+++ Problem1/Problem1/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    public class Account
+    {
+        int money;
+        public  Account ( int amount)
+        {
+
+
+            money = amount;
+
+        }
+        public void Deposit ( int amount)
+        {
+            money += amount;
+        }
+        public void withdraw(int amount)
+        {
+            money -= amount;
+
+          }
+          public int Balance()
+          {
+             return money;
+           }
+    }
+}
---
+++ Problem1/UnitTestProject1/UnitTestAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem1;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestAccount
+    {
+        [TestMethod]
+        public void TestMethodAccount()
+        {
+            Account acc = new Account(200000);
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(200000, acc.Balance());
+        }
+        public void TestMethodDeposit()
+        {
+            Account acc = new Account(300000);
+            acc.Deposit(300000);
+            Assert.AreEqual(600000, acc.Balance());
+            acc.Deposit(300000);
+            Assert.AreEqual(900000, acc.Balance());
+            
+        }
+        public void TestMethodwithdraw()
+        {
+            Account acc = new Account(500000);
+            acc.withdraw(200000);
+            Assert.AreEqual(300000, acc.Balance());
+            acc.withdraw(200000);
+            Assert.AreEqual(100000, acc.Balance());
+
+        }
+        public void TestMethodBalance(){
+            Account acc = new Account(500000);
+            acc.Deposit(200000);
+            Assert.AreEqual(700000, acc.Balance());
+            acc.withdraw(200000);
+            Assert.AreEqual(500000, acc.Balance());
+        }
+
+    }
+}
